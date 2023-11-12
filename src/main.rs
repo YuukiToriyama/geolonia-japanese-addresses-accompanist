00c3ff56b50b25056732ce741fd1bafd8ddbba4e
Yuuki Toriyama <github@toriyama.dev>
2023-11-12 10:15:57 +0900

    `/api/ja.json`をHashMapで受け取るコードを非同期関数で書き直した

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::collections::HashMap;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    const GEOLONIA_API_MASTER: &str = "https://geolonia.github.io/japanese-addresses/api/ja.json";
-    let response = reqwest::blocking::get(GEOLONIA_API_MASTER).unwrap();
-    let json = response.json::<HashMap<String, Vec<String>>>().unwrap();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let json = fetch_geolonia_api_master().await.unwrap();
     println!("{:#?}", json);
     Ok(())
+}
+
+async fn fetch_geolonia_api_master() -> Result<HashMap<String, Vec<String>>, Box<dyn std::error::Error>> {
+    const GEOLONIA_API_MASTER: &str = "https://geolonia.github.io/japanese-addresses/api/ja.json";
+    let response = reqwest::get(GEOLONIA_API_MASTER).await.unwrap();
+    let json = response.json::<HashMap<String, Vec<String>>>().await.unwrap();
+    Ok(json)
 }
