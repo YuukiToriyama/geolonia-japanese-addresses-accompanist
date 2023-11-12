1452280eff7526c77cafc54106f5cabbab21663a
Yuuki Toriyama <github@toriyama.dev>
2023-11-12 10:09:54 +0900

    `/api/ja.json`をHashMapで受け取るコードを書いた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const GEOLONIA_API_MASTER: &str = "https://geolonia.github.io/japanese-addresses/api/ja.json";
+    let response = reqwest::blocking::get(GEOLONIA_API_MASTER).unwrap();
+    let json = response.json::<HashMap<String, Vec<String>>>().unwrap();
+    println!("{:#?}", json);
+    Ok(())
 }
