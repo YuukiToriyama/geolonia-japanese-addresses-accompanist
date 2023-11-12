b3b68b93da41bfe8934383d196ab589f3adf5623
Yuuki Toriyama <github@toriyama.dev>
2023-11-12 10:42:11 +0900

    fetch_geolonia_api_master()がHashMapではなく、Vec<Prefecture>を返すように修正

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, vec};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -7,9 +7,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn fetch_geolonia_api_master() -> Result<HashMap<String, Vec<String>>, Box<dyn std::error::Error>> {
+async fn fetch_geolonia_api_master() -> Result<Vec<Prefecture>, Box<dyn std::error::Error>> {
     const GEOLONIA_API_MASTER: &str = "https://geolonia.github.io/japanese-addresses/api/ja.json";
     let response = reqwest::get(GEOLONIA_API_MASTER).await.unwrap();
-    let json = response.json::<HashMap<String, Vec<String>>>().await.unwrap();
-    Ok(json)
+    let json = response
+        .json::<HashMap<String, Vec<String>>>()
+        .await
+        .unwrap();
+    let mut prefecture_list: Vec<Prefecture> = vec![];
+    for (prefecture_name, city_list) in json {
+        prefecture_list.push(Prefecture {
+            name: prefecture_name,
+            cities: city_list,
+        })
+    }
+    Ok(prefecture_list)
+}
+
+#[derive(Debug)]
+struct Prefecture {
+    name: String,
+    cities: Vec<String>,
 }
